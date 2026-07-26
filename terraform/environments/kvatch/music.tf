9980f2669311b7533d0fb34ed78ddcf2064a8ad9
Phill Hood <phill@phillhood.ca>
2026-07-25 22:09:27 -0400

    add music lxc ct 101 to kvatch environment

---
+++ terraform/environments/kvatch/music.tf
@@ -0,0 +1,20 @@
+module "music" {
+  source = "../../modules/proxmox-lxc"
+
+  node_name        = "kvatch"
+  vm_id            = 101
+  hostname         = "music"
+  template_file_id = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
+  os_type          = "debian"
+
+  ip_address = "192.168.1.102/24"
+  gateway    = "192.168.1.1"
+
+  cores     = 2
+  memory    = 4096
+  disk_size = 20
+
+  ssh_public_keys = var.ssh_public_keys
+
+  tags = ["media", "storage"]
+}
